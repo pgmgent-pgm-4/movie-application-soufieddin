8a2b0599a229d4a897252ded7d18cd9b91d5e823
soufieddin <68068622+soufieddin@users.noreply.github.com>
2021-05-24 22:00:35 +0200

    update

--- src/app/components/DetailElement.js
+++ src/app/components/DetailElement.js
@@ -1,22 +1,27 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-
+import styles from './DetailElement.module.scss';
+const base_img_url = 'https://image.tmdb.org/t/p/original/';
 const DetailElement = () => {
+  const [element, setElement] = useState({});
   const params = useParams();
   let url = (`https://api.themoviedb.org/3/${params.type}/${params.id}?api_key=8aa3a6efa82617beae9311a4b0ef1f76&language=en-US`);
   useEffect(() => {
     async function fechData () {
       const request = await axios.get(url);
-      console.log(request.data);
+      console.log(request);
+      setElement(request.data);
       return request;
     }
     fechData();
-  },[url])
+  },[url]);
 
   return (
-    <div>
-      <h2>xxx</h2>
+    <div className = {styles.detail}>
+      <img src={`${base_img_url}${element.backdrop_path || element.poster_path}`} alt='movie poster' />
+      <h2>{element.title || element.original_name || element.original_title}</h2>
+      <p>{element.overview}</p>
     </div>
   )
 }
