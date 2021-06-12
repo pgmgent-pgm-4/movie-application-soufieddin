19a25448b6124b23cdc1fe0e9f25cdc4be6faa93
soufieddin <eddin2209@gmail.com>
2021-06-12 19:36:16 +0200

    fix deploy fout

--- client/src/app/components/detailElement/DetailElement.js
+++ client/src/app/components/detailElement/DetailElement.js
@@ -58,8 +58,8 @@ const DetailElement = () => {
       </div>
       }
         <p className={styles.detail__keys}>{words ? 'Keywords:' : ''}</p>
-        <ul className={classNames(styles.detail__keywords, `${theme === 'dark' ? styles.detail__keywords__dark : styles.detail__keywords__light}`)}>{isWordsLoading || !resultsWords ? <div>Loading...</div> : resultsWords.map(k => (<li key={k.id}><a href={`/filter/${k.name}/${type}/${k.id}`} id={k.id}>{k.name}</a></li>))}</ul>
-        <ul className={classNames(styles.detail__cast, `${theme === 'dark' ? styles.detail__cast__dark : styles.detail__cast__light}`)}>{isCastLoading || !resultsCast ? <div>Loading...</div> : resultsCast.filter(c => c.profile_path).map(c => (<li key={c.id}><a href={`/filter/${c.name}?type=movie&personId=${c.id}`}><img src={`${base_img_url}${c.profile_path}`} alt="" /><span>{c.name}</span></a></li>))}</ul>
+        <ul className={classNames(styles.detail__keywords, `${theme === 'dark' ? styles.detail__keywords__dark : styles.detail__keywords__light}`)}>{isWordsLoading || !resultsWords ? <div>Loading...</div> : resultsWords.map(k => (<li key={k.id}><a href={`/filterWord/${k.name}/${type}/${k.id}`} id={k.id}>{k.name}</a></li>))}</ul>
+        <ul className={classNames(styles.detail__cast, `${theme === 'dark' ? styles.detail__cast__dark : styles.detail__cast__light}`)}>{isCastLoading || !resultsCast ? <div>Loading...</div> : resultsCast.filter(c => c.profile_path).map(c => (<li key={c.id}><a href={`/filterActuer/${c.name}/${type}/${c.id}`}><img src={`${base_img_url}${c.profile_path}`} alt="" /><span>{c.name}</span></a></li>))}</ul>
     </div>
     </div>
   )
